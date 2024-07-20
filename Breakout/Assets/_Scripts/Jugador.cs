448ce5e1649f21cfa2cf68c5011ceec8103c5464
AntonioCarrenoF <antonio234@icloud.com>
2024-07-20 12:53:35 -0600

    Creación código jugador
    Se realizó el código para mover el jugador con la cámara.

--- Breakout/Assets/_Scripts/Jugador.cs
+++ Breakout/Assets/_Scripts/Jugador.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 23;
+    Vector3 mousePos2d;
+    Vector3 mousePos3d;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,24 @@ public class Jugador : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // cualquier codigo que se ejecute para input system o manager, realizar desden update
     {
-        
+        mousePos2d = Input.mousePosition;
+        mousePos2d.z = -Camera.main.transform.position.z;
+        mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2d);
+
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos;
     }
 }
+
