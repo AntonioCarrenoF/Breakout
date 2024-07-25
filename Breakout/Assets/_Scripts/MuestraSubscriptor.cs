f3da0a95c91c63d8fc5c593bda51b0cb220cbc65
AntonioCarrenoF <antonio234@icloud.com>
2024-07-24 20:04:20 -0600

    Creación de scripts de Eventos y Modificación de la interfáz
    Se crearon eventos con scripts
    Se crearon eventos con el editor de unity
    Se modificó la interfaz de juego

---
+++ Breakout/Assets/_Scripts/MuestraSubscriptor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuestraSubscriptor : MonoBehaviour
+{
+
+    MuestraEventos Subscriptor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Subscriptor = GetComponent<MuestraEventos>();
+        Subscriptor.EnCasoDeEspacioPresionado += MensajeEscuchadoPorElSubscriptor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MensajeEscuchadoPorElSubscriptor(object sender, EventArgs e)
+    {
+        Debug.Log("el evento ha sido escuchado desde otra clase");
+        Subscriptor.EnCasoDeEspacioPresionado -= MensajeEscuchadoPorElSubscriptor;
+    }
+}
