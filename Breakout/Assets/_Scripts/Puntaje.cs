3b7b2bfb21eb17f6e16844bb46e340ab67f31c93
AntonioCarrenoF <antonio234@icloud.com>
2024-07-24 23:56:41 -0600

    Creación Script Puntaje
    Se creó el código para los puntajes del videojuego

---
+++ Breakout/Assets/_Scripts/Puntaje.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Puntaje : MonoBehaviour
+{
+    public Transform transformPuntajeAlto;
+    public Transform transformPuntajeActual;
+    public TMP_Text textoPuntajeAlto;
+    public TMP_Text textoPuntajeActual;
+    public int puntos=0;
+    public int puntajeAlto=10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transformPuntajeActual = GameObject.Find("PuntajeActual").transform;
+        transformPuntajeAlto = GameObject.Find("PuntajeAlto").transform;
+        textoPuntajeActual = transformPuntajeActual.GetComponent<TMP_Text>();
+        textoPuntajeAlto = transformPuntajeAlto.GetComponent<TMP_Text>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        puntos += 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textoPuntajeActual.text = $"PuntajeActual: {puntos}";
+        if (puntos > puntajeAlto)
+        {
+            puntajeAlto = puntos;
+            textoPuntajeAlto.text = $"PuntajeAlto: {puntajeAlto}";
+        }
+
+    }
+}
